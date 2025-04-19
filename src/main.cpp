87c48b6ad852308c2ad0fdb17a1ab13d4725725e
Denys Hrin <denyshrin01@gmail.com>
2025-04-19 19:21:06 +0300

    image loading + textures creation

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,9 @@
 #include <iostream>
 #include <sstream>
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
 #define Assert(condition, message, ...) \
     do { \
         if (!(condition)) { \
@@ -97,10 +100,11 @@ int main( void ) {
   printf( "OpenGL version supported %s.\n", glGetString( GL_VERSION ) );
 
   float points[] = {
-    // positions         // colors
-    0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
-    -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
-     0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top 
+    // positions          // colors           // texture coords
+    0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+    0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+   -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+   -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
   };
 
   unsigned int indices[] = {  // note that we start from 0!
@@ -119,20 +123,66 @@ int main( void ) {
   glBindBuffer( GL_ARRAY_BUFFER, vbo );
   
   // position attribute
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
   glEnableVertexAttribArray(0);
   // color attribute
-  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3* sizeof(float)));
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3* sizeof(float)));
   glEnableVertexAttribArray(1);
+  // texture coord attribute
+  glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+  glEnableVertexAttribArray(2);  
 
-  /*unsigned int EBO;
-  glGenBuffers(1, &EBO);
+  unsigned int ebo;
+  glGenBuffers(1, &ebo);
 
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW); */
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-  //glDeleteVertexArrays( 1, &vao );
-  //glDeleteBuffers( 1, &vbo );
+  // TEXTURES
+  unsigned int texture1, texture2;
+  glGenTextures(1, &texture1);
+  glBindTexture(GL_TEXTURE_2D, texture1);
+  
+  // set the texture wrapping/filtering options (on the currently bound texture object)
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  // load and generate the texture
+  int width, height, nrChannels;
+  stbi_set_flip_vertically_on_load(true);  
+  unsigned char *data = stbi_load("../assets/textures/container.jpg", &width, &height, &nrChannels, 0);
+  if (data)
+  {
+      glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+      glGenerateMipmap(GL_TEXTURE_2D);
+  }
+  else
+  {
+      std::cout << "Failed to load texture" << std::endl;
+  }
+  
+  stbi_image_free(data);
+
+  glGenTextures(1, &texture2);
+  glBindTexture(GL_TEXTURE_2D, texture2);
+
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+  data = stbi_load("../assets/textures/awesomeface.png", &width, &height, &nrChannels, 0);
+  if (data)
+  {
+      glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+      glGenerateMipmap(GL_TEXTURE_2D);
+  }
+  else
+  {
+      std::cout << "Failed to load texture" << std::endl;
+  }
+  stbi_image_free(data);
 
   Shader shader_program("triangle.vert", "triangle.frag");
 
@@ -142,10 +192,11 @@ int main( void ) {
   //glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nrAttributes);
   //std::cout << "Maximum nr of vertex attributes supported: " << nrAttributes << std::endl;
   //glfwSwapInterval( 1 );
+  shader_program.use();
+  shader_program.setInt("texture1", 0);
+  shader_program.setInt("texture2", 1);
 
   while ( !glfwWindowShouldClose( window ) ) {
-    glfwPollEvents(); // Update window events.
-
     processInput(window);
     
     double curr_s     = glfwGetTime();   // Get the current time.
@@ -159,7 +210,7 @@ int main( void ) {
 
       // Create a string and put the FPS as the window title.
       char tmp[256];
-      sprintf( tmp, "FPS %.2lf", fps );
+      sprintf( tmp, "FPS: %.2lf %.2lf ms", fps, elapsed_s );
       glfwSetWindowTitle(window, tmp );
       title_countdown_s = 0.2;
     }
@@ -174,17 +225,24 @@ int main( void ) {
     glClearColor( 0.2f, 0.3f, 0.3f, 1.0f);
     glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
 
-    shader_program.use();
-    
     glBindVertexArray( vao );
-    glDrawArrays( GL_TRIANGLES, 0, 3 );
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+    //glDrawArrays( GL_TRIANGLES, 0, 3 );
     //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); // draw in wireframe mode
     //glPolygonMode(GL_FRONT_AND_BACK, GL_FILL); // dram  filled
-    //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
     //glBindVertexArray(0);
     // Put the stuff we've been drawing onto the visible area.
     glfwSwapBuffers( window );
+    glfwPollEvents(); // Update window events.
   }
+  // de-allocate all resources once they've outlived their purpose:
+  glDeleteVertexArrays(1, &vao);
+  glDeleteBuffers(1, &vbo);
+  glDeleteBuffers(1, &ebo);
   // Close OpenGL window, context, and any other GLFW resources.
   glfwTerminate();
   return 0;
